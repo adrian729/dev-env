4ea6b9ca4425e624707c4034df441584d66a8d9d
adrian729 <adrian.sanchez.albanell@gmail.com>
2025-04-13 14:32:46 +0200

    latest

--- dev-env.sh
+++ dev-env.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# script to create/copy config files
+
 script_dir="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 dry="0"
 
--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# script to run executables, to install dependencies or add configurations
+
 script_dir="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 filter=""
 dry="0"
